3802e8c4897145b04c0637e012cb29f60723263b
bardockAnes <139508042+bardockAnes@users.noreply.github.com>
2024-01-13 23:34:05 +0100

    mathjax and tippy

---
+++ javascript/math.js
@@ -0,0 +1,7 @@
+import MathJax from 'mathjax';
+
+  const math = `
+   <script type="math/tex">
+   x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}
+   </script>`;
+document.getElementById('math-display').innerHTML = "155151";
